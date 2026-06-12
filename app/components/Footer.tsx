15ffcda388066a978f0da4aecf41925c24163025
Textingman <matt.morf@textvolt.com>
2026-06-12 17:14:58 -0500

    Update footer address and add support phone number

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -74,7 +74,10 @@ export default function Footer() {
               support@themailboxjournal.com
             </p>
             <p className="mt-2" style={{ color: '#99F6E4' }}>
-              United States
+              8 The Green, Ste A, Dover, DE 19901
+            </p>
+            <p className="mt-2" style={{ color: '#99F6E4' }}>
+              (512) 998-5886
             </p>
           </div>
         </div>
